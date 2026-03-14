d8c83314836b696f90b4340baeac484a0344478d
TsarSimeon <simeoninc73@gmail.com>
2026-03-14 08:52:05 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Simple-Standalone-2D-Vector-class-C-
+A lightweight standalone 2D vector class written in C++ for use in small projects/game dev
+
+Features:
+-> Vector addition and subtraction
+-> Scalar multiplication and division
+-> Dot product
+-> Length calculation
+-> Normalization
+-> Compound operators (+=, -=, *=, /=)
+
+Usage:
+include in your project
+#include "Vec2.hpp"
+
+How to build:
+No need to build it's header-only file
+
+Linking:
+Automatic as long as the compiler knows where the header is (e.g. via include path)
---
+++ Vec2.hpp
@@ -0,0 +1,53 @@
+#pragma once
+#include <cmath>
+
+class Vec2 {
+  public:
+  float x;
+  float y;
+  Vec2() : x{0}, y{0} {}
+  Vec2(float x, float y) : x{x}, y{y} {}
+  inline Vec2 operator+(const Vec2 &v) const {
+    return {x + v.x, y + v.y};
+  }
+  inline Vec2 operator-(const Vec2 &v) const {
+    return {x - v.x, y - v.y};
+  }
+  inline Vec2 operator*(float f) const {
+    return {x * f, y * f};
+  }
+  inline Vec2 operator/(float f) const {
+    return {x / f, y / f};
+  }
+  inline Vec2 & operator+=(const Vec2 &v) {
+    x += v.x;
+    y += v.y;
+    return *this;
+  }
+  inline Vec2 & operator-=(const Vec2 &v) {
+    x -= v.x;
+    y -= v.y;
+    return *this;
+  }
+  inline Vec2 & operator*=(float f) {
+    x *= f;
+    y *= f;
+    return *this;
+  }
+  inline Vec2 & operator/=(float f) {
+    x /= f;
+    y /= f;
+    return *this;
+  }
+  inline float dot(const Vec2& v) const {
+    return x * v.x + y * v.y; 
+  }
+  inline float length() const {
+    return std::sqrt(x * x + y * y);
+  }
+  inline Vec2 normalized() const {
+    float len = length();
+    if (len < 1e-6f) return {0.0f, 0.0f};
+    return  {x/len, y/len};
+  }
+};
